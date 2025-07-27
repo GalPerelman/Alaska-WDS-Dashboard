8d482e14d649fabf2126ba163e37241536817a38
GalPerelman <peligur@gmail.com>
2025-07-27 11:33:35 +0300

    add water storage page

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from pages.raw_data import raw_data_page
 from pages.pump_curves import pump_curves_page
 from pages.demand_pattern import demands_page
 from pages.water_losses import water_losses_page
+from pages.storage import storage_page
 
 st.set_page_config(page_title="Alaska Dashboard", layout="wide")
 
-pg_main = st.Page(main_page)
+pg_main = st.Page(main_page, title="Home")
 pg_raw = st.Page(raw_data_page, title="Raw Data")
 pg_pumps = st.Page(pump_curves_page, title="Pump Curves")
 pg_demand = st.Page(demands_page, title="Demand Patterns")
 pg_water_losses = st.Page(water_losses_page, title="Water Losses")
+pg_storage = st.Page(storage_page, title="Storage")
 
-nav = st.navigation([pg_main, pg_raw, pg_pumps, pg_demand, pg_water_losses])
+nav = st.navigation([pg_main, pg_raw, pg_pumps, pg_demand, pg_water_losses, storage_page])
 
 # Track current page in session_state
 if "current_page" not in st.session_state:
@@ -22,7 +24,7 @@ if "current_page" not in st.session_state:
 
 
 st.title("Alaska WDS Data Dashboard")
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
     if st.button("Raw Datar", use_container_width=True):
@@ -36,6 +38,9 @@ with col3:
 with col4:
     if st.button("Water Losses", use_container_width=True):
         st.switch_page(pg_water_losses)
+with col5:
+    if st.button("Storage", use_container_width=True):
+        st.switch_page(pg_storage)
 
 
 nav.run()
---
+++ pages/storage.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+import graph_utils
+
+
+def storage_page():
+    st.title("Storage Level")
+
+    data = pd.read_csv("data/Arctic Village_water_level_data.csv", index_col=0)
+
+    fig = graph_utils.plot_time_series(
+        data=data,
+        data_col_names=["water_level_m"],
+        line_kw=dict(line_width=1.6))
+
+    threshold = data["critical_threshold_m"].iloc[0]
+    fig.add_hline(
+        y=threshold,
+        line=dict(color="red", dash="dash"),
+        annotation_text=f"Critical ({threshold:.2f} m)",
+        annotation_position="top left"
+    )
+
+    # Identify contiguous True stretches
+    # Every time the mask flips (True to False or False to True) we start a new group
+    mask = data["water_level_m"] < threshold
+    data["group"] = (mask != mask.shift()).cumsum()
+
+    violation_ranges = (
+        data.loc[mask]
+        .groupby("group")
+        .apply(lambda g: pd.Series({
+            "x0": g.index[0],  # first timestamp in the group
+            "x1": g.index[-1]  # last  timestamp in the group
+        }))
+    )
+
+    for _, row in violation_ranges.iterrows():
+        fig.add_shape(
+            type="rect",
+            x0=row.x0, x1=row.x1,
+            xref="x",
+            y0=0, y1=1,
+            yref="paper",  # 0–1 = full vertical span
+            fillcolor="grey",  # translucent red
+            line_width=0,
+            opacity=0.3,
+            layer="below"
+        )
+
+    fig.update_xaxes(
+        title_text="Time",
+        title_font_size=16,
+        tickfont_size=14,
+    )
+    fig.update_yaxes(
+        title_text="Water Level [m]",
+        title_font_size=16,
+        tickfont_size=16,
+    )
+
+    fig.update_layout(
+        hovermode="x unified"
+    )
+    st.plotly_chart(fig)
