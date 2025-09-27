07917eecadea5d0ffb9609c9ad98c8ab3949971d
GalPerelman <peligur@gmail.com>
2025-09-27 14:55:29 +0300

    update main page, add pages icons

---
+++ utils.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import base64
+
+
+def custom_button(png_path: str, label: str, button_id: str):
+    with open(png_path, "rb") as f:
+        data = f.read()
+    encoded = base64.b64encode(data).decode()
+
+    # CSS + HTML template
+    button_html = f"""
+        <style>
+        .custom-button-{button_id} {{
+            
+            display: flex;
+            flex-direction: column;   /* stack vertically */
+            align-items: center;      /* center horizontally */
+            justify-content: center;  /* center vertically */
+            background-color: white;
+            color: white;
+            padding: 12px 20px;
+            border: grey 1px solid;
+            border-radius: 8px;
+            text-decoration: none;
+            font-size: 20px;
+            cursor: pointer;
+            text-align: center;
+
+        }}
+        .custom-button-{button_id} img {{
+            height: 100px;
+        }}
+        </style>
+        <a class="custom-button-{button_id}" target="_self" href="?clicked={button_id}">
+            <img src="data:image/png;base64,{encoded}">
+            {label}
+        </a>
+    """
+    st.markdown(button_html, unsafe_allow_html=True)
